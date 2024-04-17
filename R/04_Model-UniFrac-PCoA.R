ba4912a953862c5ee9016088a6dc7399bcac89d9
Gusinac <Alem.Gusinac@radboudumc.nl>
2024-04-17 17:00:25 +0200

    added analysis, changed wunifrac to ps_pcoa

--- R/04_Model-UniFrac-PCoA.R
+++ R/04_Model-UniFrac-PCoA.R
@@ -2,7 +2,7 @@
 radboud_ps_rel_bac_norm <- readRDS("../../Pathology/projects/FFPE_breast_microbiome/data/RDS/02_ps_rel_bac_norm.rds")
 
 # PCoA with weighted UniFrac Analysis -----------------------------------------#
-comb_plot <- wunifrac_pcoa(radboud_ps_rel_bac_norm)
+comb_plot <- ps_pcoa(radboud_ps_rel_bac_norm)
 
 ggsave(
   filename = "04_pcoa_permanova.png",
