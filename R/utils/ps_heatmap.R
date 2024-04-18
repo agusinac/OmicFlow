bc8956342310352cc1b9c747a3aa36e593ab9300
Gusinac <Alem.Gusinac@radboudumc.nl>
2024-04-18 16:05:21 +0200

    Improved code, check line 34

--- R/utils/ps_heatmap.R
+++ R/utils/ps_heatmap.R
@@ -1,6 +1,7 @@
-ps_fold_heatmap <- function(ps, threshold = 0.2, taxa_rank = "Genus", negative_ct="healthy", positive_ct="tumor") {
+ps_heatmap <- function(ps, threshold = 0.2, taxa_rank = "Genus", RANKSTAT_col = "RANKSTAT_treatment", condition_A = "tumor", condition_B = "tumor") {
   # Fetch otus and filter out less representative Genera
-  otu_tab <- ps2otu(ps, tax_target = taxa_rank) %>% t()
+  otu_tab <- get_otu(ps, tax_target = taxa_rank) %>% t()
+  meta_tab <- get_meta(ps)
   otu_tab.filt <- otu_tab[rowSums(otu_tab) > threshold, ]
   
   # log2 scale
@@ -24,26 +25,27 @@ ps_fold_heatmap <- function(ps, threshold = 0.2, taxa_rank = "Genus", negative_c
   
   # Removes uncultured genera, cleans sample names
   df_final <- df_melt %>% 
-    filter(!grepl("uncultured", Genera)) %>% 
+    filter(!grepl("uncultured", Taxa)) %>% 
+    rowwise() %>% 
     mutate(
-      # Specific for data, atm hardcoded but should be automated!
-      group = ifelse(base::grepl("T_", variable), "tumor",
-                     ifelse(base::grepl("H_", variable), "healthy", NA)),
+      # Collects sample group names from metadata 
+      group = meta_tab[[ {{ RANKSTAT_col }} ]][str_detect(as.character(meta_tab$SAMPLE.ID), as.character(variable))],
+      # Metadata should have a column of identical sample names for paired samples!
       sample.id = sub("(-?\\w{1}_\\d+)$", "" ,variable)
     ) %>% 
     group_by(sample.id) %>% 
-    filter(any(group == negative_ct) & any(group == positive_ct))
+    filter(any(group == {{condition_A}}) & any(group == {{condition_B}}))
   
   # Computes log2(tumor) - log2(healthy)
   df_diff <- df_final %>% 
-    group_by(sample.id, Genera) %>% 
-    summarise(diff = value[group == positive_ct] - value[group == negative_ct])
+    group_by(sample.id, Taxa) %>% 
+    summarise(diff = value[group == {{condition_A}}] - value[group == {{condition_B}}])
   
   # Generate heatmap plot with df_diff data
   heatmap_plot <- df_final %>% 
     left_join(df_diff, by = c("Taxa", "sample.id")) %>% 
     ggplot(mapping = aes(x = as.factor(sample.id),
-                         y = Genera,
+                         y = Taxa,
                          fill = diff)) +
     geom_tile() +
     theme_bw() +
@@ -54,26 +56,26 @@ ps_fold_heatmap <- function(ps, threshold = 0.2, taxa_rank = "Genus", negative_c
                          low = "blue",
                          mid = "white",
                          high = "red") +
-    scale_y_discrete(limits = rev(levels(as.factor(df_final$Genera)))) +
+    scale_y_discrete(limits = rev(levels(as.factor(df_diff$Taxa)))) +
     labs(x = "Samples", 
          y = "Taxa")
   
   # Fetch otu table for boxplot and reshapes into long table
   otu_tab.tumor <- as.data.frame(otu_tab.filt)
-  otu_tab.tumor$Genera <- rownames(otu_tab.filt)
+  otu_tab.tumor$Taxa <- rownames(otu_tab.filt)
   otu_tab.tumor_ <- reshape2::melt(otu_tab.tumor, id.vars = c("Taxa"))
   
   rel_abun_plot <- otu_tab.tumor_ %>% 
     ggplot(mapping = aes(x = value,
-                         y = Genera)) +
+                         y = Taxa)) +
     geom_boxplot() +
     theme_bw() +
     theme(axis.title.y = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.y = element_blank()) +
     scale_x_continuous(breaks=seq(0, 1, 0.5)) +
-    scale_y_discrete(limits = rev(levels(as.factor(df_final$Genera)))) +
-    labs(x = "Relative Abundance")
+    scale_y_discrete(limits = rev(levels(as.factor(df_diff$Taxa)))) +
+    labs(x = "Rel. Abun.")
   
   comb_plot <- (heatmap_plot + rel_abun_plot) +
     plot_layout(guides = "collect",
