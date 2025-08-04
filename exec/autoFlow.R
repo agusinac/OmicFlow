4eee2289296b0070d2d1c3b39fcd1e0c523a17c7
agusinac <alem.gusinac@gmail.com>
2025-08-04 15:44:14 +0200

    restructed code, added new workflow, should make differentiation between metataxonomics and metagenomics, issue #8

--- exec/autoFlow.R
+++ exec/autoFlow.R
@@ -8,19 +8,24 @@ options <- parse_commandline()
 
 # main -------------------------------------------------------------------------
 # switch statement based on omic selected, create object
-omics <- switch(
-    options$omics,
-    "metagenomics" = metagenomics$new(
-        metaData = options$metadata,
-        biomData = options$biom,
-        treeData = ifelse(!is.null(options$tree), options$tree, NA)
-        )
-)
-
-# Perform automated analysis
-omics$autoFlow(
+if (options$omics == "metagenomics") {
+  tax <- metagenomics$new(
+    metaData = options$metadata,
+    biomData = options$biom,
+    treeData = ifelse(!is.null(options$tree), options$tree, NULL)
+  )
+  tax$feature_subset(Kingdom == "Bacteria")
+  tax$normalize()
+  
+  # Perform automated analysis
+  tax$autoFlow(
     beta_div_table = options$`i-beta-div`,
     alpha_div_table = options$`i-alpha-div`,
     cpus = options$cpus,
+    normalize = FALSE,
     filename = paste0(options$outdir, "/", options$filename)
-)
+  )
+  
+} else {
+  cli::cli_alert_warning("{options$omics} did not match `metagenomics`")
+}
