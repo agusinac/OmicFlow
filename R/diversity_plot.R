b05a6c177733d1b97172cc2536c8f9e4fa4e739b
agusinac <alem.gusinac@gmail.com>
2025-11-30 17:41:57 +0100

    removed gghalves, #21

--- R/diversity_plot.R
+++ R/diversity_plot.R
@@ -111,8 +111,7 @@ diversity_plot <- function(data,
   plt <- data %>%
     ggplot(mapping = aes(x = as.factor(.data[[ col_name ]]),
                          y = .data[[ values ]])) +
-    gghalves::geom_half_boxplot() +
-    gghalves::geom_half_point_panel(aes(color = as.factor(.data[[ col_name ]]))) +
+    geom_boxplot() +
     theme_bw() +
     theme(legend.position = "none",
           text=element_text(size=14),
