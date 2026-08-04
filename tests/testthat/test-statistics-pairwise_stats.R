3179029b8e7873b59f10b9b832bc99e3df00c1de
agusinac <alem.gusinac@gmail.com>
2026-08-04 13:09:40 +0200

    excluded perm_design testing in pairwise_stats

--- tests/testthat/test-statistics-pairwise_stats.R
+++ tests/testthat/test-statistics-pairwise_stats.R
@@ -27,25 +27,25 @@ test_that("`pairwise_adonis()` -- Behavioral checks", {
   expect_equal(round(res$F.Model, 2), 1.68)
   expect_equal(round(res$R2, 2), 0.17)
 
-  ## Testing with permutation design
-  perm_design_func <- function(meta) {
-    base::with(
-      data = meta,
-      expr = permute::how(
-        nperm = 9,
-        plots = permute::Plots(meta$SAMPLEPAIR_ID, type = "none"),
-        within = permute::Within(type = "free")
-      )
-    )
-  }
-  expect_no_error(res <- pairwise_adonis(x = distmat, groups = test$metaData$CONTRAST_treatment, metadata = test$metaData, perm_design = perm_design_func))
-  expect_equal(colnames(res), c("pairs", "Df", "SumsOfSqs", "F.Model", "R2", "p.value", "p.adj"))
-  expect_equal(res$pairs, "tumor vs control")
-  expect_equal(res$Df, 1)
-  expect_equal(round(res$SumsOfSqs, 2), 1382.68)
-  expect_equal(round(res$F.Model, 2), 1.68)
-  expect_equal(round(res$R2, 2), 0.17)
-  expect_equal(res$p.value, 1)
+  # ## Testing with permutation design
+  # perm_design_func <- function(meta) {
+  #   base::with(
+  #     data = meta,
+  #     expr = permute::how(
+  #       nperm = 9,
+  #       plots = permute::Plots(meta$SAMPLEPAIR_ID, type = "none"),
+  #       within = permute::Within(type = "free")
+  #     )
+  #   )
+  # }
+  # expect_no_error(res <- pairwise_adonis(x = distmat, groups = test$metaData$CONTRAST_treatment, metadata = test$metaData, perm_design = perm_design_func))
+  # expect_equal(colnames(res), c("pairs", "Df", "SumsOfSqs", "F.Model", "R2", "p.value", "p.adj"))
+  # expect_equal(res$pairs, "tumor vs control")
+  # expect_equal(res$Df, 1)
+  # expect_equal(round(res$SumsOfSqs, 2), 1382.68)
+  # expect_equal(round(res$F.Model, 2), 1.68)
+  # expect_equal(round(res$R2, 2), 0.17)
+  # expect_equal(res$p.value, 1)
 })
 
 test_that("`pairwise_anosim()` -- Argument checks", {
@@ -67,20 +67,20 @@ test_that("`pairwise_anosim()` -- Behavioral checks", {
   expect_equal(res$pairs, "tumor vs control")
   expect_equal(round(res$anosimR, 2), 0.38)
 
-  ## Testing with permutation design
-  perm_design_func <- function(meta) {
-    base::with(
-      data = meta,
-      expr = permute::how(
-        nperm = 9,
-        plots = permute::Plots(meta$SAMPLEPAIR_ID, type = "none"),
-        within = permute::Within(type = "free")
-      )
-    )
-  }
-  expect_no_error(res <- pairwise_anosim(x = distmat, groups = test$metaData$CONTRAST_treatment, metadata = test$metaData, perm_design = perm_design_func))
-  expect_equal(colnames(res), c("pairs", "anosimR", "p.value", "p.adj"))
-  expect_equal(res$pairs, "tumor vs control")
-  expect_equal(round(res$anosimR, 2), 0.38)
-  expect_equal(res$p.value, 1)
+  # ## Testing with permutation design
+  # perm_design_func <- function(meta) {
+  #   base::with(
+  #     data = meta,
+  #     expr = permute::how(
+  #       nperm = 9,
+  #       plots = permute::Plots(meta$SAMPLEPAIR_ID, type = "none"),
+  #       within = permute::Within(type = "free")
+  #     )
+  #   )
+  # }
+  # expect_no_error(res <- pairwise_anosim(x = distmat, groups = test$metaData$CONTRAST_treatment, metadata = test$metaData, perm_design = perm_design_func))
+  # expect_equal(colnames(res), c("pairs", "anosimR", "p.value", "p.adj"))
+  # expect_equal(res$pairs, "tumor vs control")
+  # expect_equal(round(res$anosimR, 2), 0.38)
+  # expect_equal(res$p.value, 1)
 })
